9ce447586ae6986eb79ee152f135d108ec8b0bde
obowersa <owen+github@obowersa.net>
2020-05-27 05:12:55 +0100

    Initial ratelimiting implementation
    Implemented a simple channel based rate limiter. Currently:
    
    - Need to refactor huge chunks of the modQuery module
    
    -Expand testing.
    
    -Get some sleep

--- pkg/modquery/modquery.go
+++ pkg/modquery/modquery.go
@@ -1,11 +1,13 @@
 package modquery
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/obowersa/wfwiki/internal/lua"
 )
@@ -15,7 +17,7 @@ type wfmodule interface {
 	getStatsConcat(string) string
 }
 
-type wfwiki struct {
+type wikiJSON struct {
 	Query struct {
 		Pages map[string]struct {
 			Pageid    int    `json:"pageid"`
@@ -45,30 +47,124 @@ type cost struct {
 	Parts      []parts `json:"Parts,omitempty"`
 }
 
-func getWikiContent(w wfmodule) wfwiki {
-	var module wfwiki
+type wikiResponse struct {
+	response *http.Response
+	err      error
+}
 
-	req, err := http.NewRequest("GET", w.getURL(), nil)
+type wikiRequest struct {
+	url    string
+	respCh chan *wikiResponse
+	ctx    context.Context
+}
+
+type requestHandler struct {
+	client     *http.Client
+	rate       time.Duration
+	reqCh      chan *wikiRequest
+	reqTimeout time.Duration
+}
+
+func newRequestHandler() *requestHandler {
+	client := &http.Client{}
+	rate := time.Second
+	requests := make(chan *wikiRequest, 10)
+	reqTimeout := 5 * time.Second
+	r := requestHandler{client, rate, requests, reqTimeout}
+
+	go r.processRequests()
+
+	return &r
+}
+
+func (r *requestHandler) processRequests() {
+	throttle := time.NewTicker(r.rate)
+
+	for req := range r.reqCh {
+		<-throttle.C
+		//NOTE: Move getWikiContent function to be a method of requestHandler?
+		//NOTE: Reconsider this when testing turns out to be a pain
+		go r.fulfillRequest(req)
+	}
+}
+
+func (r *requestHandler) fulfillRequest(wReq *wikiRequest) {
+	//TODO: Add in hook to caching algorithm. Want to avoid parsing the lua code into the VM for every response
+	//TODO: Potentially impact cache check within process reqCh. Return cache before waiting for ticker so repeated reqCh can be fulfilled outside of the 1/s tick loop
+	res, err := r.client.Get(wReq.url)
+	//TODO: Refactor select statement based on: https://blog.golang.org/pipelines
+	select {
+	case <-wReq.ctx.Done():
+		return
+	default:
+		wReq.respCh <- &wikiResponse{res, err}
+	}
+}
+
+func (r *requestHandler) handleRequest(req *wikiRequest) (resp *wikiResponse) {
+	//TODO: Look into moving getModule func into requestHandler method sig
+	c := make(chan *wikiResponse)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	defer close(c)
+
+	r.reqCh <- &wikiRequest{req.url, c, ctx}
+	select {
+	case n := <-c:
+		cancel()
+		resp = n
+	case <-time.After(r.reqTimeout):
+		cancel()
+		//TODO: Refactor error handling
+		resp = &wikiResponse{err: fmt.Errorf("timeout waiting for response from: %s", req.url)}
+	}
+
+	return
+}
+
+type WFWiki struct {
+	httpReq *requestHandler
+}
+
+func NewWFWiki() WFWiki {
+	return WFWiki{newRequestHandler()}
+}
+
+//Refactor the below into seperate functions
+func (w *WFWiki) request(m string) (wfmodule, string, error){
+	var wikiBase wikiJSON
+
+	module, err := getModule(m)
 	if err != nil {
+		//TODO: Handle this gracefully
 		panic(err)
 	}
 
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		panic(err)
-	}
+	//Wiki request creator ?
+	req := wikiRequest{module.getURL(), nil, nil}
+	res := w.httpReq.handleRequest(&req).response
+
 	defer res.Body.Close()
-
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		panic(err)
 	}
 
-	if err := json.Unmarshal(body, &module); err != nil {
+	if err := json.Unmarshal(body, &wikiBase); err != nil {
 		panic(err)
 	}
 
-	return module
+	var l string
+
+	if len(wikiBase.Query.Pages) != 1 {
+		return module, "", fmt.Errorf("too many pages for single request")
+	}
+
+	for _, v := range wikiBase.Query.Pages {
+		l += v.Revisions[0].Data
+	}
+
+	return module, l, nil
 }
 
 func getModule(n string) (wfmodule, error) {
@@ -85,30 +181,11 @@ func getModule(n string) (wfmodule, error) {
 	}
 }
 
-func getWikiLua(m wfmodule) (string, error) {
-	var l string
-
-	wikiJSON := getWikiContent(m)
-	if len(wikiJSON.Query.Pages) != 1 {
-		return "", fmt.Errorf("too many pages for single request")
-	}
-
-	for _, v := range wikiJSON.Query.Pages {
-		l += v.Revisions[0].Data
-	}
-
-	return l, nil
-}
 
 //GetStats queries the wiki module specified by mod, and returns the stats about the object specified
 //by query
-func GetStats(mod string, query string) string {
-	module, err := getModule(mod)
-	if err != nil {
-		panic(err)
-	}
-
-	moduleLua, err := getWikiLua(module)
+func (w WFWiki) GetStats(mod string, query string) string {
+	module, moduleLua, err := w.request(mod)
 	if err != nil {
 		panic(err)
 	}
--- pkg/modquery/modquery_test.go
+++ pkg/modquery/modquery_test.go
@@ -1,8 +1,11 @@
 package modquery
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 )
 
 //TODO: Expand testing, include mock of API
@@ -44,13 +47,50 @@ func TestGetStats(t *testing.T) {
 		{"mod", "Abating Link", "Abating Link", nil},
 	}
 
+	n := NewWFWiki()
+
 	for _, tt := range getStatsTests {
 		t.Run(tt.module, func(t *testing.T) {
-			res := GetStats(tt.module, tt.query)
+			res := n.GetStats(tt.module, tt.query)
 			if !strings.HasPrefix(res, tt.expected) {
 				t.Errorf("Base result does not match query: Got: %s, Expected: %s", res, tt.expected)
 			}
 
 		})
+	}
+}
+
+//Expand testing to cover nil cases. Look into table testing and how to make use of for this
+//TODO: Add time to first byte support for testing duration of call
+func TestRequestHandler(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	defer srv.Close()
+
+	baseStart := time.Now()
+
+	for n := 1; n < 5; n++ {
+		_, err := http.Get(srv.URL)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	baseDuration := time.Since(baseStart)
+
+	c := newRequestHandler()
+	req := wikiRequest{srv.URL, nil, nil}
+	testStart := time.Now()
+
+	for n := 1; n < 5; n++ {
+		res := c.handleRequest(&req)
+		if res.err != nil {
+			t.Error(res.err)
+		}
+	}
+
+	testDuration := time.Since(testStart)
+
+	if testDuration < (baseDuration * 5) {
+		t.Errorf("rate limiting failed, testDuration: %s, baseDuration: %s", testDuration, baseDuration)
 	}
 }
