4bb8b1b7ef79d442a549fab36a4668ca5ebbd0fe
Owen Bowers Adams <owen+github@obowersa.net>
2020-06-22 04:31:13 +0100

    Fixed JSON Unmarshall bug with marketcost (#28)

--- pkg/wfwiki/shared.go
+++ pkg/wfwiki/shared.go
@@ -1,5 +1,14 @@
 package wfwiki
 
+import (
+	"encoding/json"
+	"strconv"
+)
+
+type marketCost struct {
+	MCost string
+}
+
 //Parts struct shared by multiple modules
 type parts struct {
 	Name  string `json:"Name"`
@@ -9,10 +18,26 @@ type parts struct {
 
 //Cost struct shared by multiple modules
 type cost struct {
-	Credits    int     `json:"Credits,omitempty"`
-	BPCost     int     `json:"BPCost,omitempty"`
-	MarketCost float64 `json:"MarketCost,omitempty"`
-	Rush       int     `json:"Rush,omitempty"`
-	Time       int     `json:"Time,omitempty"`
-	Parts      []parts `json:"Parts,omitempty"`
+	Credits    int        `json:"Credits,omitempty"`
+	BPCost     int        `json:"BPCost,omitempty"`
+	MarketCost marketCost `json:"MarketCost,omitempty"`
+	Rush       int        `json:"Rush,omitempty"`
+	Time       int        `json:"Time,omitempty"`
+	Parts      []parts    `json:"Parts,omitempty"`
+}
+
+func (h *marketCost) UnmarshalJSON(data []byte) error {
+	var s string
+	if err := json.Unmarshal(data, &s); err != nil {
+		var i int
+		if err := json.Unmarshal(data, &i); err != nil {
+			return err
+		}
+
+		s = strconv.Itoa(i)
+	}
+
+	h.MCost = s
+
+	return nil
 }
